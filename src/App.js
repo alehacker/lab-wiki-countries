3f194137efc58e0c3d804bd7e67367671b2706f7
Ale Kennedy <alepertuz@gmail.com>
2023-02-24 12:03:43 -0500

    Lab done, including bonus

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 
 import './App.css';
-import countries from './countries.json'
+// import countries from './countries.json'
 import Navbar from './components/Navbar'
 import { Routes, Route } from "react-router-dom"; 
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
+
+   const [countries, setCountries] = useState([])
+   useEffect (()=>{
+    
+         axios.get('https://ih-countries-api.herokuapp.com/countries')
+         .then(response => {
+
+            console.log("response", response.data)
+
+            let sorted = response.data.sort((a,b) => {
+               return a.name.common.localeCompare(b.name.common)
+            } )
+
+            // let countriesApiResponse = response.data
+
+            setCountries(sorted)
+         })
+         .catch((err) => console.log(err))
+       
+   }, [])
+
+
+
+
+   return (
     <div className="App">
       <Navbar />
-      <CountriesList allCountries = {countries} />
-      <Routes>
-         <Route path='/:countryCode' element={<CountryDetails allCountries={countries} />} />  
-      </Routes>
-
-
+      <div className="container">
+         <div className="row">
+            <div className="col-md-6">
+               <CountriesList allCountries={countries} />
+            </div>
+            <div className="col-md-6">
+               <Routes>
+                  <Route path='/:countryCode' element={<CountryDetails allCountries={countries} />} />  
+               </Routes>
+            </div>
+         </div>
+      </div>
     </div>
   );
 }
--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,21 +1,63 @@
 import { useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import countries from '../countries.json'
+import axios from 'axios';
 
 
-const CountryDetails = () =>{
+const CountryDetails = ({ allCountries }) =>{
    const [foundCountry, setFoundCountry] = useState(null);
   
   const { countryCode } = useParams();
 
-   useEffect(()=>{
+
+  const countryDetailsFromApi = (code) =>{
+   const url = 'https://ih-countries-api.herokuapp.com/countries/' + countryCode
+   axios.get(url)
+   .then(response => {
+      console.log('API call response ===>', response.data)
+      let countryInfo =(response.data) 
+      console.log(countryInfo)
+   })
+   .catch((err) => console.log(err))
+
+   
+  }
+
+
+  useEffect(() =>{
+   let countryInfo = countryDetailsFromApi(countryCode)
+   // console.log('API call ===>', countryInfo)
+   setFoundCountry(countryInfo)
+      
+  }, [countryCode])
+
+
+
+
+  const getCountry = (code) => {
+   return allCountries.find((elm) => elm.alpha3Code === code)
+  }
+
+
+   
+  useEffect(()=>{
+     
+     let found = getCountry(countryCode)
+      //  let found = allCountries.find((elm) => elm.alpha3Code === countryCode)
         
-      setFoundCountry(countries.find((elm) => elm.alpha3Code === countryCode))
-        
+      setFoundCountry(found)
+         console.log("State Found", found)
+       console.log("All", allCountries) 
+       console.log("code", countryCode)
+       console.log("Found", allCountries.find((elm) => elm.alpha3Code == countryCode))
     }, [countryCode])
 
    return(
       <div className="col-7">
+   {
+      foundCountry ?  
+         <div>
+            <img src={"https://flagpedia.net/data/flags/icon/72x54/" + foundCountry.alpha2Code.toLowerCase() + ".png"} alt="" class="img-fluid" />
             <h1>{foundCountry.name.official}</h1>
             <table className="table">
               <thead></thead>
@@ -31,23 +73,42 @@ const CountryDetails = () =>{
                     <sup>2</sup>
                   </td>
                 </tr>
-                {/* <tr> */}
-                  {/* <td>Borders</td>
+                <tr>
                   <td>
                     <ul>
-                    {foundCountry.borders.map((element))
+                    {foundCountry.borders.map((element) =>{
 
-                    }
-                      <li><a href="/AND">Andorra</a></li>
+                     return (
+                        <li>
+                     <Link to={`/${element}`} >
+                     
+                     {
+                        getCountry(element).name.common
+
+          
+                     }</Link>
+
+                        </li>
+                     )
+
+                    })}
+                    
 
                     </ul>
-                  </td> */}
-                {/* </tr> */}
+                  </td> 
+                </tr>
               </tbody>
             </table>
+
+         </div>
+      
+      : <p>Loading...</p>
+   }
       </div>
    )
 }
 
 export default CountryDetails
+
+
 
